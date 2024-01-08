9ea08881915a644e99382da6ac1d3cf1446a99a5
Shivaraj <shivaraj@someshwara.com>
2024-01-08 22:25:49 +0530

    commited 01Basics to copyOfmain

---
+++ 01Basics/01variables.js
@@ -0,0 +1,21 @@
+// console.log("someshwara software ")
+
+//var,let ,const keywords
+
+//how to store a name ?
+
+var fullName="shivaraj kumar"
+console.log(fullName)
+
+//how to store a number ?
+var age=23 ;
+console.log(age)
+
+//how to store a boolean ?
+var isLoggedIn=true
+
+// undefined
+console.log(declare)
+
+var declare
+declare=35
---
+++ 01Basics/04conditionals.js
@@ -0,0 +1,13 @@
+var temparature
+
+temparature=20;
+console.log(temparature<20)
+
+if(temparature<=20){
+    console.log("its very cold outside")
+}
+if(temparature>20){
+    console.log("its moderate here")
+}else{
+    console.log("its very hot outside")
+}
